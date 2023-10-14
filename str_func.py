14f70c478bc09170e273b6716902ed7dd33b74d0
Konstantin Onishchenko <kostik80_80@mail.ru>
2023-10-14 12:37:20 +0300

    add modified function str_upper add function str_title

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def str_func(value):
     """функция фыводит все заглавные"""
     return value.upper()
+
+def str_title(value):
+    """
+    Функция выводит первые заглавные буквы
+    """
+    return value.title()
